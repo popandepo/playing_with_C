9ad28358cb8612abc31d6c60cf64a7d2fb8e7dbd
popandepo <popandepo@gmail.com>
2024-09-07 18:38:32 +0200

    playing_with_C-1 Add Safe Create function
    added a way to create and manage new objects into memory, with proper disposal.
    
    requirements for a new dynamic struct is that it contains a MemorySafe object, an AddXToMemory function that takes in a MemoryHolder pointer and adds all containing variables to it as well as a Create function that sets the MemorySafe.AddToMemory function to call the AddXToMemory function when needed.
    
    a parent object should call the child objects' MemorySafe.AddToMemory functions

--- main.c
+++ main.c
@@ -1,38 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int GetLen(const char* str);
-int GetChars(const char* str[]);
 typedef struct Person Person;
 typedef struct Name Name;
 typedef struct MemoryHolder MemoryHolder;
+typedef struct MemorySafe MemorySafe;
 
-struct Person {
-    int ID;
-    Name* Name;
+int GetLen(const char* str);
+int GetChars(const char* str[]);
+void Add(MemoryHolder* mem, void* ptr);
+void AddPersonToMemory(void* obj, MemoryHolder* mem);
+void AddNameToMemory(void* obj, MemoryHolder* mem);
+Person* CreatePerson(int id, Name* name);
+Name* CreateName(char* first, char* middle, char* last);
+
+/// MemorySafe - a struct that contains a function pointer to add an object to memory
+/// @param AddToMemory - a function pointer to add an object to memory
+/// @return a MemorySafe struct
+/// @note this struct is used to add objects to memory
+struct MemorySafe {
+    void(*AddToMemory)(void* obj, MemoryHolder* mem);
 };
 
+/// Name - a struct that contains a first, middle, and last name
+/// @param First - the first name
+/// @param Middle - the middle name
+/// @param Last - the last name
+/// @param MemorySafe - a MemorySafe struct that contains a function pointer to add the name to memory
+/// @return a Name struct
+/// @note this struct is used to store a person's name
 struct Name {
     char* First;
     char* Middle;
     char* Last;
+    MemorySafe MemorySafe;
 };
 
 /// CreateName - creates a new name struct
 /// @param first - the first name
 /// @param middle - the middle name
 /// @param last - the last name
-/// @return the new name struct
+/// @return a new Name struct
+/// @note this function is used to create a new name struct
 Name* CreateName(char* first, char* middle, char* last) {
     Name* n = malloc(sizeof(Name));
-    n->First = first;
-    n->Middle = middle;
-    n->Last = last;
+    n->First = strdup(first);
+    n->Middle = strdup(middle);
+    n->Last = strdup(last);
+
+    n->MemorySafe.AddToMemory = AddNameToMemory;
+
     return n;
 }
 
+/// AddNameToMemory - adds a name to memory
+/// @param obj - the name to add to memory
+/// @param mem - the memory holder to add the name to
+/// @note this function is used to add a name to memory
+void AddNameToMemory(void* obj, MemoryHolder* mem) {
+    Name* n = obj;
+    if (n == NULL || mem == NULL) {
+        return;
+    }
+    Add(mem, n->First);
+    Add(mem, n->Middle);
+    Add(mem, n->Last);
+    Add(mem, n);
+}
+
+/// Person - a struct that contains an ID and a name
+/// @param ID - the ID of the person
+/// @param Name - the name of the person
+/// @param MemorySafe - a MemorySafe struct that contains a function pointer to add the person to memory
+/// @return a Person struct
+/// @note this struct is used to store a person's ID and name
+struct Person {
+    int ID;
+    Name* Name;
+    MemorySafe MemorySafe;
+};
+
+/// CreatePerson - creates a new person struct
+/// @param id - the ID of the person
+/// @param name - the name of the person
+/// @return a new Person struct
+/// @note this function is used to create a new person struct
+Person* CreatePerson(int id, Name* name) {
+    Person* p = malloc(sizeof(Person));
+    p->ID = id;
+    p->Name = name;
+
+    p->MemorySafe.AddToMemory = AddPersonToMemory;
+
+    return p;
+}
+
+/// AddPersonToMemory - adds a person to memory
+/// @param obj - the person to add to memory
+/// @param mem - the memory holder to add the person to
+/// @note this function is used to add a person to memory
+void AddPersonToMemory(void* obj, MemoryHolder* mem) {
+    Person* p = obj;
+    if (p == NULL || mem == NULL || p->Name == NULL) {
+        return;
+    }
+
+    p->Name->MemorySafe.AddToMemory(p->Name, mem);
+    Add(mem, p);
+}
+
+/// MemoryHolder - a struct that contains an array of pointers and a count of pointers
+/// @param pointers - an array of pointers
+/// @param count - the number of pointers in the array
+/// @return a MemoryHolder struct
+/// @note this struct is used to store pointers to memory for later freeing
 struct MemoryHolder {
-    void** ptr;
+    void** pointers;
     int count;
 };
 
@@ -40,79 +124,63 @@ struct MemoryHolder {
 /// @param mem - the memory holder to add the pointer to
 /// @param ptr - the pointer to add to the memory holder
 /// @return the memory holder with the new pointer added
-MemoryHolder* Add(MemoryHolder* mem, void* ptr) {
+void Add(MemoryHolder* mem, void* ptr) {
     if (mem == NULL) {
-        return NULL;
+        return;
     }
 
-    void** newArr = realloc(mem->ptr, (mem->count + 1) * sizeof(void*));
+    void** newArr = realloc(mem->pointers, (mem->count + 1) * sizeof(void*));
 
     if (newArr == NULL) {
-        return NULL;
+        return;
     }
 
-    mem->ptr = newArr;
-    mem->ptr[mem->count] = ptr;
+    mem->pointers = newArr;
+    mem->pointers[mem->count] = ptr;
     mem->count++;
-    return mem;
 }
 
+/// CreateMemoryHolder - creates a new memory holder struct
+/// @return a new MemoryHolder struct
+/// @note this function is used to create a new memory holder struct
 MemoryHolder* CreateMemoryHolder() {
     MemoryHolder* mem = malloc(sizeof(MemoryHolder));
-    mem->ptr = NULL;
+    mem->pointers = NULL;
     mem->count = 0;
     return mem;
 }
 
+/// FreeMemory - frees the memory in the memory holder
+/// @param mem - the memory holder to free the memory of
+/// @note this function is used to free the memory in the memory holder
 void FreeMemory(MemoryHolder* mem) {
     if (mem == NULL) {
         return;
     }
     for (int i = 0; i < mem->count; i++) {
-        printf("Freeing %p\n", mem->ptr[i]);
-        free(mem->ptr[i]);
+        printf("Freeing %p\n", mem->pointers[i]);
+        free(mem->pointers[i]);
     }
-    free(mem->ptr);
+    free(mem->pointers);
     free(mem);
 }
 
 int main(void) {
     MemoryHolder* mem = CreateMemoryHolder();
 
-    Person* p = malloc(sizeof(Person));
-    p->ID = 1;
-    p->Name = CreateName("John", "Doe", "Smith");
+    Person* p = CreatePerson(1, CreateName("John", "Doe", "Smith"));
+    Person* p2 = CreatePerson(2, CreateName("Jane", "Doe", "Smith"));
 
-    Person* p2 = malloc(sizeof(Person));
-    p2->ID = 2;
-    p2->Name = CreateName("Jane", "Doe", "Smith");
-
-    mem = Add(mem, p->Name);
-    mem = Add(mem, p);
-    mem = Add(mem, p2->Name);
-    mem = Add(mem, p2);
+    p->MemorySafe.AddToMemory(p, mem);
+    p2->MemorySafe.AddToMemory(p2, mem);
 
     printf("ID: %d\n", p->ID);
     printf("Name: %s %s %s\n", p->Name->First, p->Name->Middle, p->Name->Last);
 
-    char *holder[] = {"Hello", " ", "world", "!"};
-    int characters = GetChars(holder);
-    char string[characters + 1];
-    int counter = 0;
-    for (int i = 0; i < sizeof(holder)/sizeof(holder[0]); i++) {
-        char * temp = holder[i];
-        for (int j = 0; j < GetLen(temp); j++) {
-            string[counter] = temp[j];
-            counter++;
-        }
-    }
-    string[counter] = '\0';
-    printf("%s\n", string);
-
     //read memory locations
     printf("\nMemory Locations:\n");
     for (int i = 0; i < mem->count; i++) {
-        printf("Pointer %d: %p\n", i, mem->ptr[i]);
+        printf("Pointer %d: %p\n", i, mem->pointers[i]);
     }
 
     FreeMemory(mem);
